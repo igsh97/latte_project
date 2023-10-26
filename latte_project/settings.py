3df0a7c69da06444cf956ba6f4ec0488e1cc41f2
igsh97 <igsh97@naver.com>
2023-10-26 12:33:45 +0900

    change profile picture post, debug=false, alloewed host='*'로 바꿈

--- latte_project/settings.py
+++ latte_project/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-%at=d&*$ttoo*&+ygbl5d16xj$noyu8*&1t158bpu7%lnqzvj2"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
